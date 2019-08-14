82f311dd72c77a922caede7c008303faeb49c39c
Davis Hochstatter <davishochs@gmail.com>
2019-08-13 21:09:56 -0500

    none

--- assets/javascript/lottery.js
+++ assets/javascript/lottery.js
@@ -1,11 +1,13 @@
-
-
+const randomLottoNumbers = [];
 function getRandomLottoNumbers() {
-    const randomLottoNumbers = [];
+    
 
     for (let i= 0; i < 6; i++) {
         let pick = Math.floor(Math.random() * 55);
         randomLottoNumbers.push(pick);
+        console.log(randomLottoNumbers)
     }
 }
-
+getRandomLottoNumbers();
+console.log(randomLottoNumbers);
+ 
