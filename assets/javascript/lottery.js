d3afe13c77a62f6f6dd3743d38bb202fe2c9a25e
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-08-13 21:15:28 -0500

    appended random numbers

--- assets/javascript/lottery.js
+++ assets/javascript/lottery.js
@@ -1,11 +1,30 @@
-const randomLottoNumbers = [];
+
+
 function getRandomLottoNumbers() {
-    
+    const randomLottoNumbers = [];
 
     for (let i= 0; i < 6; i++) {
         let pick = Math.floor(Math.random() * 55);
         randomLottoNumbers.push(pick);
-        console.log(randomLottoNumbers)
     }
 }
+
+let numberActual =getRandomLottoNumbers();
+
+
+$('#random-button').on('click',function getRandomLottoNumbers() {
+    let randomLottoNumbers =[];
+
+    for (let i= 0; i < 6; i++) {
+        let pick = Math.floor(Math.random() * 55);
+        randomLottoNumbers.push(pick);
+    }
+    
+    $('#random-number').append($(`<div> ${randomLottoNumbers.join(' ')}</div>`))
+
+
+    
+
+});
+
 
